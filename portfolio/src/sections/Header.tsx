6fa37f08939a51e7eec607cc0ff6b2fc0c3f0dbd
Riyan <riyannazeer786@gmail.com>
2024-11-06 21:39:57 +0530

    properly formated the header and made a nice navbar

--- portfolio/src/sections/Header.tsx
+++ portfolio/src/sections/Header.tsx
@@ -1,10 +1,12 @@
 export const Header = () => {
-  return <div>
-    <nav>
-      <a href="#">Home</a>
-      <a href="#">Projects</a>
-      <a href="#">About</a>
-      <a href="#">Contact</a>
+  return (
+  <div className="flex justify-center items-center relative top-3">
+    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full">
+      <a href="#" className="nav-item">Home</a>
+      <a href="#" className="nav-item">Projects</a>
+      <a href="#" className="nav-item">About</a>
+      <a href="#" className="nav-item">Contact</a>
     </nav>
-  </div>;
+  </div>
+  );
 };
