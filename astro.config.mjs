375cc7aa05552d0b7eacc057567135f9ca4fa711
Mohammed Riyan Nazeer <riyannazeer786@gmail.com>
2025-11-21 05:34:28 +0530

    Update site and base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://astro.build/config
-export default defineConfig({
-  site: "https://riyan00b.github.io",
-  base: "/RiDevfolio",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+export default {
+  site: 'https://ricode.me',
+  base: '/',
+};
