3511d78459bb27b1da11982712f0084fdd998416
riyan <riyannazeer786@gmail.com>
2025-11-21 05:41:19 +0530

    added ssl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-export default {
-  site: 'https://ricode.me',
-  base: '/',
-};
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://ricode.me",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
