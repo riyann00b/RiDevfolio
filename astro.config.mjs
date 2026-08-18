2b89c023ff31bd2a302748405dbd2b840edfbcfb
MD Riyan Nazeer <riyannazeer786@gmail.com>
2026-08-19 04:54:12 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ricode.dev",
+  site: "https://www.ricode.dev",
   vite: {
     plugins: [tailwindcss()],
   },
